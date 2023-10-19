fa579f8744021b6a14c344117abc248369a69d54
chrystian-souza <121185752+chrystian-souza@users.noreply.github.com>
2023-10-18 21:21:00 -0300

    commit3

--- conf/settings.py
+++ conf/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # MY APPS
+    # MY APPS ############################################AQUI#####foi consigurado para rodar os apps criados
     'stockADS'
 ]
 
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], ############################AQUI######## foi configurado
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static') ########################AQUI#######Foi consigurado para puxar CSS
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
