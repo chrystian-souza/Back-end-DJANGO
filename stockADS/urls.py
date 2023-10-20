78abe3fd8658c44e390064137bd61c8db6be93ac
chrystian-souza <121185752+chrystian-souza@users.noreply.github.com>
2023-10-19 21:35:17 -0300

    'commit-de-hoje'

--- stockADS/urls.py
+++ stockADS/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='home')
-]
+    path('', views.index, name='home'),
+    path('product-detail/<int:id>', views.product_detail, name='product-detail')
+    
+]  
+
+ 
--- stockADS/views.py
+++ stockADS/views.py
@@ -1,11 +1,12 @@
-
-
 from django.shortcuts import render, HttpResponse
-from .models import Products
+from .models import Products 
 
 def index(request):
 
-    produtos = Products.objects.get(id=1)
+    produtos = Products.objects.all()
     print(produtos)
-    return render(request, 'pages/index.html', {'produtos': produtos})
+    return render(request, 'pages/index.html', {'produtos':produtos})
 
+def product_detail(request, id):
+    product = Products.objects.get(id=id)
+    return render(request, 'pages/product_detail.html', {'product':product}) #####  {'product':product}) serve para registrar o produto para poder visualizar na tela
