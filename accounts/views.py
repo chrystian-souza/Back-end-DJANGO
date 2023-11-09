ddbdf63767bc1f6024ba91d49ee55a87c2cc1c1c
Chrystian <Chrystian.souza.silveira@gmail.com>
2023-11-08 21:09:41 -0300

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ def user_login(request):
         userVerify = auth.authenticate(request, username=user, password=password)
        
         if userVerify == None:
+            messages.info(request, "Usuário ou senha incorretos.")
             return redirect('login')
 
         else:
